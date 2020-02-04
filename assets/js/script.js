6df9918f329b19d0fee5b4ea422cf08ee44446f2
joao luiz <joao_luiz92@hotmail.com>
2020-02-03 21:04:38 -0300

    responsividade 80%

--- assets/js/script.js
+++ assets/js/script.js
@@ -73,3 +73,23 @@ if(elementos.length){
 $(function(){
     $('[data-toggle="tooltip"]').tooltip();
 })
+
+
+
+function toggleMenu(){
+    let menu = document.getElementById('menu');
+
+    if(menu.style.display == 'none' || menu.style.display == ''){
+        
+        menu.style.display = "block";
+        
+
+        
+        
+    }else{
+        menu.style.display = "none";
+        
+        
+    }
+}
+
